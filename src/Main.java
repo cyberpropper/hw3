ce253b3967d65ee254fffb2797ceba233acee488
cyberpropper <osankin2002@mail.ru>
2024-05-10 23:40:24 +0700

    1лвл

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20000000;
+        byte b = -128;
+        float c = 3.452f;
+        short d = -32768;
+        long  e = -9223372036854775807L;
+        double f = -1.7976931348623157E308;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
